1484948011d5b01a1ff3f446e0d5f0900eb9a973
deyvidCh <deyvidchinchilla2@gmail.com>
2024-08-03 21:14:56 -0600

    Tarea 0 Desarrollo Web

--- enviar.js
+++ enviar.js
@@ -1,5 +1,4 @@
 function showAlert() {
-    // Obtener los valores de los campos del formulario
     const Nombres = document.getElementById('Nombres').value;
     const Apellidos = document.getElementById('Apellidos').value;
     const NumeroTel = document.getElementById('NumeroTelefonico').value;
@@ -7,9 +6,7 @@ function showAlert() {
     const Fecha = document.getElementById('Fecha').value;
     const Mensaje = document.getElementById('Mensaje').value;
 
-    // Crear el mensaje del alert
     const alertMessage = `Nombres: ${Nombres}\nApellidos: ${Apellidos}\nNumeroTelefonico: ${NumeroTel}\nCorreoElectronico: ${Correo}\nFecha: ${Fecha}\nMensaje: ${Mensaje}`;
 
-    // Mostrar el alert con los datos ingresados
     alert(alertMessage);
 }
